fa44a91751b6753bb32a5ff0339e155f9a7c0e85
Marco Novas <marco.nicolas.novas@gmail.com>
2026-06-14 23:54:36 -0300

    router connection added to main

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import ProductsRouter from "../src/routes/product.routes.js";
+
 const app = express();
 
 app.use(cors());
 
+app.use("/api/products", ProductsRouter);
 
 const PORT = 3000 || process.env.PORT;
 
