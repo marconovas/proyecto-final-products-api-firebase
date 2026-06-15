e1184f089ac01dab73a8bd9ddbc19118ca9ec52d
Marco Novas <marco.nicolas.novas@gmail.com>
2026-06-15 00:09:56 -0300

    body-parser added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import ProductsRouter from "../src/routes/product.routes.js";
 import { notFound } from "../src/middlewares/notFound.middleware.js";
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.use("/api/products", ProductsRouter);
 
